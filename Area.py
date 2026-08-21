b0367c74b70e1f29272809cb455dc6637eec9aca
senna2303 <alanfelix2@gmail.com>
2026-08-20 17:42:12 -0700

    test commit

--- Area.py
+++ Area.py
@@ -3,10 +3,21 @@
 def calculate_rectange(lenght,width):
     area = lenght * width
     return area
+
+
+
+
 def main():
+    """Main function to parse commmand-line arguments 
+    and calculate the area"""
     parse = argparse.ArgumentParser(description="Calculate the areas of a rectangle")
     parse.add_argument("Lenght",type=float,help="Lenght of the rectangle")
     parse.add_argument("Width",type=float,help="Width of the rectangle")
-    args = parse.parse_args()
+    args = parser.parse_args()
+
     area = calculate_rectange(args.Lenght,args.Width)
     print(f"The area of the rectangle with a length of {args.Lenght} and a width of {args.Width} is {area}")
+
+
+if __name__ == "__main__":
+    main()
